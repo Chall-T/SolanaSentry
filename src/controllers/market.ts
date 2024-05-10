4cab60832c3bd698220bda768205bd273392dc7e
Chall-T <gdav476@gmail.com>
2024-05-10 12:02:33 +0200

    fetching rest of dex exchanges

--- src/controllers/market.ts
+++ src/controllers/market.ts
@@ -8,7 +8,22 @@ export const getLpByTokenPlane = async (token: string) => {
     const timeStart = Date.now();
     const query = /* GraphQL */ `
       query MyQuery($where: Raydium_LiquidityPoolv4_bool_exp) {
-        Raydium_LiquidityPoolv4(where: $where) {
+        Raydium_LiquidityPoolv4(
+          where: {
+            _or: [
+              {
+                baseMint: {
+                  _eq: "${token}"
+                }
+              }
+              {
+                quoteMint: {
+                  _eq: "${token}"
+                }
+              }
+            ]
+          }
+        ) {
           _updatedAt
           amountWaveRatio
           baseDecimal
@@ -63,19 +78,121 @@ export const getLpByTokenPlane = async (token: string) => {
           withdrawQueue
           pubkey
         }
+        ORCA_WHIRLPOOLS_whirlpool(
+          where: {
+            _or: [
+              {
+                tokenMintA: {
+                  _eq: "${token}"
+                }
+              }
+              {
+                tokenMintB: {
+                  _eq: "${token}"
+                }
+              }
+            ]
+          }
+        ) {
+          whirlpoolsConfig
+          whirlpoolBump
+          tokenVaultB
+          tokenVaultA
+          tokenMintB
+          tokenMintA
+          tickSpacingSeed
+          tickSpacing
+          tickCurrentIndex
+          sqrtPrice
+          rewardLastUpdatedTimestamp
+          rewardInfos
+          pubkey
+          protocolFeeRate
+          protocolFeeOwedB
+          protocolFeeOwedA
+          liquidity
+          feeRate
+          feeGrowthGlobalB
+          feeGrowthGlobalA
+          _lamports
+        }
+        meteora_dlmm_LbPair(
+          where: {
+            _or: [
+              {
+                tokenXMint: {
+                  _eq: "${token}"
+                }
+              }
+              {
+                tokenYMint: {
+                  _eq: "${token}"
+                }
+              }
+            ]
+          }
+        ) {
+          whitelistedWallet
+          vParameters
+          tokenYMint
+          tokenXMint
+          swapCapDeactivateSlot
+          status
+          rewardInfos
+          reserved
+          reserveY
+          reserveX
+          pubkey
+          protocolFee
+          parameters
+          pairType
+          padding1
+          oracle
+          maxSwappedAmount
+          lastUpdatedAt
+          feeOwner
+          bumpSeed
+          binStepSeed
+          binStep
+          baseKey
+          binArrayBitmap
+          activeId
+          activationSlot
+          _lamports
+        }
+        Fluxbeam_TokenSwap(
+          where: {
+            _or: [
+              { mintB: { _eq: "${token}" } }
+              { mintA: { _eq: "${token}" } }
+            ]
+          }
+        ) {
+          version
+          tradeFeeNumerator
+          tradeFeeDenominator
+          tokenPool
+          tokenAccountB
+          tokenAccountA
+          pubkey
+          poolTokenProgramId
+          ownerWithdrawFeeDenominator
+          ownerWithdrawFeeNumerator
+          ownerTradeFeeNumerator
+          ownerTradeFeeDenominator
+          mintB
+          mintA
+          isInitialized
+          hostFeeNumerator
+          hostFeeDenominator
+          curveType
+          feeAccount
+          curveParameters
+          bumpSeed
+        }
       }
     `;
 
-    const variables = {
-      where: {
-        _or: [
-          { baseMint: { _eq: token } },
-          {
-            quoteMint: { _eq: token },
-          },
-        ],
-      },
-    };
     const res = await fetch(
       `https://programs.shyft.to/v0/graphql/?api_key=${process.env.SHYFT_API_KEY}`,
       {
@@ -86,14 +203,13 @@ export const getLpByTokenPlane = async (token: string) => {
         },
         body: JSON.stringify({
           query,
-          variables: variables,
         }),
       }
     );
-    const data = await res.json();
+    const data: any = await res.json();
     let response: any = {
       statusCode: 200,
-      data,
+      data: data["data"],
     };
     const timeStop = Date.now();
     if (process.env.NODE_ENV === "development") {
